74436147d8aa83c54c65d859655e277b84ae1031
mp5maker <khan.photon@gmail.com>
2018-07-22 16:44:22 +0600

    Database + Migration

--- database/migrations/2018_07_22_103348_create_users_table.php
+++ database/migrations/2018_07_22_103348_create_users_table.php
@@ -15,10 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name', 128);
+            $table->string('email', 128);
+            $table->string('password', 128);
+            $table->string('thumbnail', 128);
             $table->timestamps();
         });
     }

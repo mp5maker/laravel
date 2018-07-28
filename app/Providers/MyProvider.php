46f841e4395f2ba1b8b48b22c46cf24d168b41f6
mp5maker <khan.photon@gmail.com>
2018-07-28 16:21:40 +0600

    Provider Update

---
+++ app/Providers/MyProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class MyProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->router->get('/providerservice', function(){
+            return "Simple Provider Service Test";
+        });
+    }
+}
